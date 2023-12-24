a011d163d7501e91c574b1f1dcf6f739b954ca83
Tyuiu.MotorovaDD@outlook.com <Tyuiu.MotorovaDD@outlook.com>
2023-12-24 13:37:29 +0500

    1

--- Tyuiu.MotorovaDD.Sprint7.V15.Test/DataServiceTest.cs
+++ Tyuiu.MotorovaDD.Sprint7.V15.Test/DataServiceTest.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace Tyuiu.MotorovaDD.Sprint7.V15
+namespace Tyuiu.MotorovaDD.Sprint7.V15.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
--- Tyuiu.MotorovaDD.Sprint7.V15.Test/Properties/AssemblyInfo.cs
+++ Tyuiu.MotorovaDD.Sprint7.V15.Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tyuiu.MotorovaDD.Sprint7.V15")]
+[assembly: AssemblyTitle("Tyuiu.MotorovaDD.Sprint7.V15.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Tyuiu.MotorovaDD.Sprint7.V15")]
+[assembly: AssemblyProduct("Tyuiu.MotorovaDD.Sprint7.V15.Test")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("cfa5768c-8421-4473-bba0-df8a5cacb2f6")]
+[assembly: Guid("e3cec49c-89af-48a6-b9c4-68b7018e6fc4")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
